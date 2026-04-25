395903aa7b04e1cb8f6319a5733ae11b0d8c0a19
Fish <377182330@qq.com>
2026-04-25 18:32:18 +0800

    # rd132211 + 仅复制rd132211

---
+++ src/main/java/com/fish/mcclone/level/LevelListener.java
@@ -0,0 +1,10 @@
+package com.fish.mcclone.level;
+
+public interface LevelListener {
+    void tileChanged(int paramInt1, int paramInt2, int paramInt3);
+
+    void lightColumnChanged(int paramInt1, int paramInt2, int paramInt3, int paramInt4);
+
+    void allChanged();
+}
+
