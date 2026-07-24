c2514acfce49f657811b2f5e545fad536d5d7cf6
Fish <377182330@qq.com>
2026-07-24 20:17:02 +0800

    + 准星JavaDoc

--- src/main/java/com/fish/arcaterra/ui/hud/Crosshair.java
+++ src/main/java/com/fish/arcaterra/ui/hud/Crosshair.java
@@ -6,6 +6,7 @@ import static org.lwjgl.opengl.GL11.*;
 import static com.fish.arcaterra.Arcaterra.WIDTH;
 import static com.fish.arcaterra.Arcaterra.HEIGHT;
 
+/// ## 准星
 public class Crosshair implements IHudElement {
     @Override
     public void render() {
