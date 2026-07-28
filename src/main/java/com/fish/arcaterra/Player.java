29a0763e9295ebc50c9fe3de01d3f217e6e8a2b8
Fish <377182330@qq.com>
2026-07-28 17:56:12 +0800

    re 可以修改世界中心；那tm是在天上

--- src/main/java/com/fish/arcaterra/Player.java
+++ src/main/java/com/fish/arcaterra/Player.java
@@ -45,7 +45,7 @@ public class Player implements IDebugWindowPrintRegistry {
         this.world = world;
         this.xRot = 0;
         this.yRot = 0;
-        setPos(0, 0, 50);
+        setPos(0, 0, world.getTerrainProvider().getHeight(0,0));
         debugParamRegister(); // 自动注册调试信息
     }
 
