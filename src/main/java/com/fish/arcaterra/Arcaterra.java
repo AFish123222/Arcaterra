b28d5a06a36068010ee7d4096c0295c6e076d444
Fish <377182330@qq.com>
2026-07-28 14:07:12 +0800

    tring debug坐标指示器；；备份

--- src/main/java/com/fish/arcaterra/Arcaterra.java
+++ src/main/java/com/fish/arcaterra/Arcaterra.java
@@ -245,50 +245,6 @@ public class Arcaterra {
 
             particlePool.render(player.x, player.y, player.z);
 
-            //////////
-            float eyeY = 0;
-            glPushMatrix();
-            glDisable(GL_LIGHTING);
-            glDisable(GL_TEXTURE_2D);
-            glLineWidth(2.5f);
-
-            // X轴（红）
-            glColor3f(1.0f, 0.0f, 0.0f);
-            glBegin(GL_LINES);
-            glVertex3f(0, eyeY, 0);
-            glVertex3f(1.2f, eyeY, 0);
-            glEnd();
-
-            // Y轴（绿）
-            glColor3f(0.0f, 1.0f, 0.0f);
-            glBegin(GL_LINES);
-            glVertex3f(0, eyeY, 0);
-            glVertex3f(0, eyeY + 1.2f, 0);
-            glEnd();
-
-            // Z轴（蓝）
-            glColor3f(0.0f, 0.0f, 1.0f);
-            glBegin(GL_LINES);
-            glVertex3f(0, eyeY, 0);
-            glVertex3f(0, eyeY, 1.2f);
-            glEnd();
-
-            // 恢复状态
-            glEnable(GL_LIGHTING);
-            glEnable(GL_TEXTURE_2D);
-            glPopMatrix();
-
-            glPointSize(6.0f);
-            glBegin(GL_POINTS);
-            glColor3f(1,0,0);
-            glVertex3f(1.2f, eyeY, 0);
-            glColor3f(0,1,0);
-            glVertex3f(0, eyeY + 1.2f, 0);
-            glColor3f(0,0,1);
-            glVertex3f(0, eyeY, 1.2f);
-            glEnd();
-            //////////
-
             DebugIndicators.getDebugIndicators().setPlayerPos(player.x, player.y , player.z);
             DebugIndicators.getDebugIndicators().setRotation(player.yRot, player.xRot);
 
