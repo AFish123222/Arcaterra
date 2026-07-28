98ca7c3c3886d50986092b6997e3f7f9887372a7
Fish <377182330@qq.com>
2026-07-28 16:34:53 +0800

    tring 高程顶点采样lod;备份；bug没解决:仅地底可见附近;; 背面剔除？？？；

--- src/main/java/com/fish/arcaterra/Arcaterra.java
+++ src/main/java/com/fish/arcaterra/Arcaterra.java
@@ -325,7 +325,7 @@ public class Arcaterra {
                 int idx = 0;
                 for (float z = minZ; z <= maxZ; z += step) {
                     for (float x = minX; x <= maxX; x += step) {
-                        float h = world.getTerrainProvider().getHeight(x, z);
+                        float h = world.getTerrainProvider().getHeight(x, z)+100f;
                         vArr[idx++] = x;
                         vArr[idx++] = h;
                         vArr[idx++] = z;
