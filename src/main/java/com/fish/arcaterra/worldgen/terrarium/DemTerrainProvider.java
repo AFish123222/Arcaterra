e41e430e9493a84b758e9c3b5ff4ebbc7e25ec74
Fish <377182330@qq.com>
2026-07-30 01:11:59 +0800

    backup;

--- src/main/java/com/fish/arcaterra/worldgen/terrarium/DemTerrainProvider.java
+++ src/main/java/com/fish/arcaterra/worldgen/terrarium/DemTerrainProvider.java
@@ -50,7 +50,7 @@ public class DemTerrainProvider implements TerrainProvider {
         try {
             Files.createDirectories(cacheDir);
         } catch (IOException e) {
-            System.err.println("无法创建缓存目录: " + cacheRoot);
+            System.err.println("Can't : " + cacheRoot + " because "+e.getMessage());
         }
 
         this.originLon = originLon;
